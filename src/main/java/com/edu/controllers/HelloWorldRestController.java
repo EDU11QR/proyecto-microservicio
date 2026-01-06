2b43dbdd128e2cf673d5145f2f20d64d6dbf659c
edu <edu@gmail.com>
2026-01-06 18:17:51 -0500

    Inicialización del microservicio con Spring Boot

---
+++ src/main/java/com/edu/controllers/HelloWorldRestController.java
@@ -0,0 +1,15 @@
+package com.edu.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping({"/hello", "/hw", "/hola"})
+    public String helloWorld(){
+        System.out.println("Solicitud ejecutada!");
+        return  "Hello, World!";
+    }
+
+}
