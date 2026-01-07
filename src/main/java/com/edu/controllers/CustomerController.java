e34cd83db99aa400f2ff32917c77487f12bb330c
edu <edu@gmail.com>
2026-01-07 18:57:24 -0500

    feat: implementamos Las anotaciones  @DeleteMapping ,@PatchMapping y  @RequestMapping  Eliminamos las rutas de nuestros metodos   ya que requestMapping unifica todos

--- src/main/java/com/edu/controllers/CustomerController.java
+++ src/main/java/com/edu/controllers/CustomerController.java
@@ -8,6 +8,9 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+// Eliminamos las rutas de nuestros metodos
+// ya que requestMapping unifica todos
+@RequestMapping("/clientes")
 public class CustomerController {
 
     // Creamos una lista de clientes
@@ -18,13 +21,14 @@ public class CustomerController {
             new Customer(159,"Yhanis Basilio", "yhanis","159")
     ));
 
-    @GetMapping("/clientes")
+    //Metodo Get para listar Clientes
+    @GetMapping //("/clientes")
     public List<Customer> getCustomers(){
         return customers;
     }
 
     // Enrutamiento de URL
-    @GetMapping({"/clientes/{username}"})
+    @GetMapping({"/{username}"})
     public Customer getCliente(@PathVariable String username){
         for(Customer c : customers) {
             if (c.getUsername().equalsIgnoreCase(username)){
@@ -34,50 +38,65 @@ public class CustomerController {
         return null;
     }
 
-    @PostMapping("/clientes")
+    //Metodo Post para crear un nuevo Cliente
+    @PostMapping
     public Customer postCliente(@RequestBody Customer customer){
         customers.add(customer);
         return  customer;
     }
 
-}
+    //Metodo Put para Actualizar/Modificar un cliente
+    @PutMapping
+    public  Customer putCliente(@RequestBody Customer customer){
+        for(Customer c : customers){
+            if (c.getID() == customer.getID()){
+                c.setName(customer.getName());
+                c.setUsername(customer.getUsername());
+                c.setPassword(customer.getPassword());
+
+                return c;
+            }
+        }
+        return null;
+    }
+
+    //Metodo Delete para Eliminar un cliente
+    @DeleteMapping("/{id}")
+    public Customer deleteCliente(@PathVariable int id){
+        for (Customer c : customers){
+            if(c.getID() == id){
+                customers.remove(c);
+
+                return c;
+            }
+        }
+        return null;
+    }
+
+    // Metodo Patch se usa para actualizar parcialmente un recurso
+    // no todo el objeto a diferencia del put
+    @PatchMapping
+    public Customer patchCliente(@RequestBody Customer customer){
+        for(Customer c : customers){
+            if (c.getID() == customer.getID()){
+
+                if (customer.getName() != null){
+                    c.setName(customer.getName());
+                }
+
+                if (customer.getUsername() != null){
+                    c.setUsername(customer.getUsername());
+                }
+
+                if (customer.getPassword() != null){
+                    c.setPassword(customer.getPassword());
+                }
+
+                return c;
+            }
+        }
+        return null;
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}// fin del Controlador
